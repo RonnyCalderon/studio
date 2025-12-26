d1c2c00af741aff1012af6ad8b54d15a0a92625a
Closed Account <ro94ca@gmail.com>
2025-12-26 01:55:28 +0000

    quotes

--- src/lib/quotes.ts
+++ src/lib/quotes.ts
@@ -1,42 +1,34 @@
 export const quotes = [
   {
-    text: "The quality of your life ultimately depends on the quality of your relationships.",
-    author: "Esther Perel"
+    text: "She pushed his hand away, took his sex into her mouth again, and with her two hands she encircled his sexual parts, caressed him and absorbed him until he came. He leaned over with gratitude, tenderness, and murmured, “You are the first woman, the first woman, the first woman…”",
+    author: "Anaïs Nin (Delta of Venus)"
   },
   {
-    text: "Love is a verb. Not a permanent state of enthusiasm.",
-    author: "Esther Perel"
+    text: "Her two hands were as active as her mouth. The titillation almost deprived each man of his senses. The elasticity of her hands; the variety of rhythms; the change from a hand grip of the entire penis to the lightest touch of the tip of it.",
+    author: "Anaïs Nin (Delta of Venus)"
   },
   {
-    text: "Confidence is the sexiest thing a woman can have. It's much sexier than any body part.",
-    author: "Aimee Mullins"
+    text: "I pull him deeper into my mouth so I can feel him at the back of my throat and then to the front again. My tongue swirls around the end. He’s my very own Christian Grey-flavored popsicle. I suck harder and harder.…",
+    author: "E.L. James (Fifty Shades of Grey)"
   },
   {
-    text: "The best thing to hold onto in life is each other.",
-    author: "Audrey Hepburn"
+    text: "He was the kind of guy who made a woman want to rip his shirt open and watch the buttons scatter along with her inhibitions. I looked at him in his civilized, urbane, outrageously expensive suit and thought of raw, primal, sheet-clawing fucking.",
+    author: "Sylvia Day (Bared to You)"
   },
   {
-    text: "Sex is not a performance, not a skill, not a game at which you win or lose. It's a journey of exploration.",
-    author: "Emily Nagoski, 'Come as You Are'"
+    text: "Only the united beat of sex and heart together can create ecstasy.",
+    author: "Anaïs Nin (Delta of Venus)"
   },
   {
-    text: "Each time you love, love as deeply as if it were forever.",
-    author: "Audre Lorde"
+    text: "He was now in that state of fire that she loved. She wanted to be burnt.",
+    author: "Anaïs Nin (Delta of Venus)"
   },
   {
-    text: "I am a lover without a lover. I am lovely and lonely and I belong deeply to myself.",
-    author: "Anaïs Nin"
+    text: "I had a feeling that Pandora’s box contained the mysteries of woman’s sensuality, so different from a man’s, and for which man’s language was so inadequate. The language of sex had yet to be invented. The language of the senses was yet to be explored.",
+    author: "Anaïs Nin (Delta of Venus)"
   },
   {
-    text: "The erotic is a resource within each of us that lies in a deeply female and spiritual plane, firmly rooted in the power of our unexpressed or unrecognized feeling.",
-    author: "Audre Lorde, 'Uses of the Erotic'"
-  },
-  {
-    text: "The most courageous act is still to think for yourself. Aloud.",
-    author: "Coco Chanel"
-  },
-  {
-    text: "Your pleasure is the point. Healthy sexual pleasure is a crucial component of healthy sexuality, and healthy sexuality is a crucial component of a healthy life.",
-    author: "Emily Nagoski, 'Come as You Are'"
+    text: "I want to fall in love in such a way that the mere sight of a man, even a block away from me, will shake and pierce me, will weaken me, and make me tremble and soften and melt.",
+    author: "Anaïs Nin (Delta of Venus)"
   }
-];
+]

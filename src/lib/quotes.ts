cea1e584052152b2a7cf56eefd6fef5696b39bbc
Closed Account <ro94ca@gmail.com>
2025-12-26 04:49:22 +0000

    quote

--- src/lib/quotes.ts
+++ src/lib/quotes.ts
@@ -1,82 +1,106 @@
 export const quotes = [
   {
-    text: "She pushed his hand away, took his sex into her mouth again, and with her two hands she encircled his sexual parts, caressed him and absorbed him until he came. He leaned over with gratitude, tenderness, and murmured, “You are the first woman, the first woman, the first woman…”",
-    author: "Anaïs Nin (Delta of Venus)"
+    "text": "She pushed his hand away, took his sex into her mouth again, and with her two hands she encircled his sexual parts, caressed him and absorbed him until he came. He leaned over with gratitude, tenderness, and murmured, “You are the first woman, the first woman, the first woman…”",
+    "author": "Anaïs Nin (Delta of Venus)"
   },
   {
-    text: "Her two hands were as active as her mouth. The titillation almost deprived each man of his senses. The elasticity of her hands; the variety of rhythms; the change from a hand grip of the entire penis to the lightest touch of the tip of it.",
-    author: "Anaïs Nin (Delta of Venus)"
+    "text": "Her two hands were as active as her mouth. The titillation almost deprived each man of his senses. The elasticity of her hands; the variety of rhythms; the change from a hand grip of the entire penis to the lightest touch of the tip of it.",
+    "author": "Anaïs Nin (Delta of Venus)"
   },
   {
-    text: "He was now in that state of fire that she loved. She wanted to be burnt.",
-    author: "Anaïs Nin (Delta of Venus)"
+    "text": "He was now in that state of fire that she loved. She wanted to be burnt.",
+    "author": "Anaïs Nin (Delta of Venus)"
   },
   {
-    text: "Only the united beat of sex and heart together can create ecstasy.",
-    author: "Anaïs Nin (Delta of Venus)"
+    "text": "Only the united beat of sex and heart together can create ecstasy.",
+    "author": "Anaïs Nin (Delta of Venus)"
   },
   {
-    text: "I had a feeling that Pandora’s box contained the mysteries of woman’s sensuality, so different from a man’s, and for which man’s language was so inadequate. The language of sex had yet to be invented. The language of the senses was yet to be explored.",
-    author: "Anaïs Nin (Delta of Venus)"
+    "text": "I had a feeling that Pandora’s box contained the mysteries of woman’s sensuality, so different from a man’s, and for which man’s language was so inadequate. The language of sex had yet to be invented. The language of the senses was yet to be explored.",
+    "author": "Anaïs Nin (Delta of Venus)"
   },
   {
-    text: "Without leaving his armchair, without even touching her with his fingertips, he ordered her to kneel down in front of him, take him and caress his sex until he discharged in her mouth.",
-    author: "Pauline Réage (Story of O)"
+    "text": "Without leaving his armchair, without even touching her with his fingertips, he ordered her to kneel down in front of him, take him and caress his sex until he discharged in her mouth.",
+    "author": "Pauline Réage (Story of O)"
   },
   {
-    text: "Keep me rather in this cage, and feed me sparingly, if you dare. Anything that brings me closer to illness and the edge of death makes me more faithful. It is only when you make me suffer that I feel safe and secure.",
-    author: "Pauline Réage (Story of O)"
+    "text": "Keep me rather in this cage, and feed me sparingly, if you dare. Anything that brings me closer to illness and the edge of death makes me more faithful. It is only when you make me suffer that I feel safe and secure.",
+    "author": "Pauline Réage (Story of O)"
   },
   {
-    text: "If the costume we wear in the evening leaves our sex exposed, it is not for the sake of convenience, but for the sake of insolence, so that your eyes will be directed there upon it and nowhere else, so that you may learn that there resides your master, for whom, above all else, your lips are intended.",
-    author: "Pauline Réage (Story of O)"
+    "text": "If the costume we wear in the evening leaves our sex exposed, it is not for the sake of convenience, but for the sake of insolence, so that your eyes will be directed there upon it and nowhere else, so that you may learn that there resides your master, for whom, above all else, your lips are intended.",
+    "author": "Pauline Réage (Story of O)"
   },
   {
-    text: "I want it to happen in my presence, I want her to do it as I wish, I want her to give herself where I give myself. It’s via Hélène Lagonelle’s body, through it, that the ultimate pleasure would pass from him to me. A pleasure unto death.",
-    author: "Marguerite Duras (The Lover)"
+    "text": "I want it to happen in my presence, I want her to do it as I wish, I want her to give herself where I give myself. It’s via Hélène Lagonelle’s body, through it, that the ultimate pleasure would pass from him to me. A pleasure unto death.",
+    "author": "Marguerite Duras (The Lover)"
   },
   {
-    text: "He was the kind of guy who made a woman want to rip his shirt open and watch the buttons scatter along with her inhibitions. I looked at him in his civilized, urbane, outrageously expensive suit and thought of raw, primal, sheet-clawing fucking.",
-    author: "Sylvia Day (Bared to You)"
+    "text": "He was the kind of guy who made a woman want to rip his shirt open and watch the buttons scatter along with her inhibitions. I looked at him in his civilized, urbane, outrageously expensive suit and thought of raw, primal, sheet-clawing fucking.",
+    "author": "Sylvia Day (Bared to You)"
   },
   {
-    text: "I pull him deeper into my mouth so I can feel him at the back of my throat and then to the front again. My tongue swirls around the end. He’s my very own Christian Grey-flavored popsicle. I suck harder and harder.…",
-    author: "E.L. James (Fifty Shades of Grey)"
+    "text": "I pull him deeper into my mouth so I can feel him at the back of my throat and then to the front again. My tongue swirls around the end. He’s my very own Christian Grey‑flavored popsicle. I suck harder and harder.…",
+    "author": "E.L. James (Fifty Shades of Grey)"
   },
   {
-    text: "We fucked in bathrooms and alleys bold as boys, bent over porcelain sinks... When someone saw us by accident, I let them be embarrassed.",
-    author: "Michelle Tea (Valencia)"
+    "text": "We fucked in bathrooms and alleys bold as boys, bent over porcelain sinks... When someone saw us by accident, I let them be embarrassed.",
+    "author": "Michelle Tea (Valencia)"
   },
   {
-    text: "I want to feel you deep inside me. I want to hear you groan and pant and moan as you plunge deeper and deeper.",
-    author: "Pepper Winters"
+    "text": "I want to feel you deep inside me. I want to hear you groan and pant and moan as you plunge deeper and deeper.",
+    "author": "Pepper Winters"
   },
   {
-    text: "Beauty, you must learn it. You must accept and yield, and then you shall see everything is simple.",
-    author: "Anne Rice (The Claiming of Sleeping Beauty)"
+    "text": "Beauty, you must learn it. You must accept and yield, and then you shall see everything is simple.",
+    "author": "Anne Rice (The Claiming of Sleeping Beauty)"
   },
   {
-    text: "This is me, Ana. All of me...and I'm all yours. What do I have to do to make you realize that? To make you see that I want you any way I can get you.",
-    author: "E.L. James (Fifty Shades of Grey)"
+    "text": "This is me, Ana. All of me...and I'm all yours. What do I have to do to make you realize that? To make you see that I want you any way I can get you.",
+    "author": "E.L. James (Fifty Shades of Grey)"
   },
   {
-    text: "You forget who submits, Eva,” he said gruffly. “I've given up control for you. I've bent and adjusted for you. I'll do anything to keep you and make you happy.",
-    author: "Sylvia Day (Bared to You)"
+    "text": "You forget who submits, Eva,” he said gruffly. “I've given up control for you. I've bent and adjusted for you. I'll do anything to keep you and make you happy.",
+    "author": "Sylvia Day (Bared to You)"
   },
   {
-    text: "I want it to happen in my presence, I want her to do it as I wish, I want her to give herself where I give myself. It’s via Hélène Lagonelle’s body, through it, that the ultimate pleasure would pass from him to me. A pleasure unto death.",
-    author: "Marguerite Duras (The Lover)"
+    "text": "I want it to happen in my presence, I want her to do it as I wish, I want her to give herself where I give myself. It’s via Hélène Lagonelle’s body, through it, that the ultimate pleasure would pass from him to me. A pleasure unto death.",
+    "author": "Marguerite Duras (The Lover)"
   },
   {
-    text: "I had a feeling that Pandora’s box contained the mysteries of woman’s sensuality, so different from a man’s, and for which man’s language was so inadequate. The language of sex had yet to be invented. The language of the senses was yet to be explored.",
-    author: "Anaïs Nin (Delta of Venus)"
+    "text": "I had a feeling that Pandora’s box contained the mysteries of woman’s sensuality, so different from a man’s, and for which man’s language was so inadequate. The language of sex had yet to be invented. The language of the senses was yet to be explored.",
+    "author": "Anaïs Nin (Delta of Venus)"
   },
   {
-    text: "Only the united beat of sex and heart together can create ecstasy.",
-    author: "Anaïs Nin (Delta of Venus)"
+    "text": "Only the united beat of sex and heart together can create ecstasy.",
+    "author": "Anaïs Nin (Delta of Venus)"
   },
   {
-    text: "The three of us,” Sean murmured.",
-    author: "Melanie Hitchens (Handled: At The Night Club Series: Book 2)"
+    "text": "The three of us,” Sean murmured.",
+    "author": "Melanie Hitchens (Handled: At The Night Club Series: Book 2)"
   },
+  {
+    "text": "I came with a rough cry and an eager thrust of my hips. Thrusting up with such a force I felt the bite of his upper teeth against the smooth skin of my mound.",
+    "author": "Sommer Marsden (Appetizer from The Sexy Librarian’s Big Book of Erotica)"
+  },
+  {
+    "text": "His mouth clamped down on me, his lips soft and hot. His tongue parted me, slickening my already juicy pussy with his saliva. His tongue painted insistent swirls on my clitoris until I was gripping the arms of the chair and moving my body up to meet him.",
+    "author": "Sommer Marsden (Appetizer from The Sexy Librarian’s Big Book of Erotica)"
+  },
+  {
+    "text": "You definitely have the voice for phone sex, but I've got to go. I have a date with my vibrator.",
+    "author": "Sylvia Day (Bared to You)"
+  },
+  {
+    "text": "That is your life now, and you should think of nothing else, and regret nothing else. I want that dignity peeled away from you as if it were so many skins of the onion. I don’t mean that you should ever be graceless. I mean that you should surrender to me.",
+    "author": "Anne Rice (The Claiming of Sleeping Beauty)"
+  },
+  {
+    "text": "I want to feel you deep inside me. I want to hear you groan and pant and moan as you plunge deeper and deeper.",
+    "author": "Pepper Winters"
+  },
+  {
+    "text": "I think my heart knew you were mine long before I ever realized it.",
+    "author": "Sarah J. Maas, A Court of Frost and Starlight "
+  }
 ]
